7b8ba4f33df4f533ccb360d1878c82fc7d15747a
James McDowell <jamesmcdowell@iheartmedia.com>
2021-06-25 02:27:26 +0000

    added parameter to override app settings

--- sanantonioscientist/app.py
+++ sanantonioscientist/app.py
@@ -2,17 +2,27 @@ from flask import Flask
 from sanantonioscientist.blueprints.page import page
 
 
-def create_app():
-    """
-    Create a Flask application using the app factory pattern.
+def create_app(settings_override=None):
+    """Generate a Flask application using the app factory pattern.
 
-    :return: Flask app
-    """
+    Parameters
+    ----------
+    settings_override : dict, optional
+        Config override settings, by default None
+
+    Returns
+    -------
+    flask.app.Flask
+        An instance of our flask app
+    """    
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_object('config.settings')
     app.config.from_pyfile('settings.py', silent=True)
 
+    if settings_override:
+        app.config.update(settings_override)
+    
     app.register_blueprint(page)
 
     return app
