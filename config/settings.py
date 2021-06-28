5b44ccd5271c932a45b2d0e60a653770beeda73a
James McDowell <jamesmcdowell@iheartmedia.com>
2021-06-28 16:18:48 +0000

    changed depracated celery settings

--- config/settings.py
+++ config/settings.py
@@ -16,8 +16,8 @@ MAIL_PASSWORD = 'awesomepassword'
 
 # Celery.
 CELERY_BROKER_URL = 'redis://:devpassword@redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://:devpassword@redis:6379/0'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_REDIS_MAX_CONNECTIONS = 5
+RESULT_BACKEND = 'redis://:devpassword@redis:6379/0'
+ACCEPT_CONTENT = ['json']
+TASK_SERIALIZER = 'json'
+RESULT_SERIALIZER = 'json'
+REDIS_MAX_CONNECTIONS = 5
