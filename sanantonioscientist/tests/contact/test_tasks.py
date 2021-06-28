90461239b7a09b625352507ee1049447b1519f4e
James McDowell <jamesmcdowell@iheartmedia.com>
2021-06-28 17:24:59 +0000

    added test to make sure template gets rendered in email

---
+++ sanantonioscientist/tests/contact/test_tasks.py
@@ -0,0 +1,17 @@
+from sanantonioscientist.extensions import mail
+from sanantonioscientist.blueprints.contact.tasks import deliver_contact_email
+
+
+class TestTasks:
+    def test_deliver_support_email(self):
+        """Deliver a contact email."""
+        form = {
+            'email': 'foo@bar.com',
+            'message': 'Test message from San Antonio Scientist.'
+        }
+
+        with mail.record_messages() as outbox:
+            deliver_contact_email(form.get('email'), form.get('message'))
+
+            assert len(outbox) == 1
+            assert form.get('email') in outbox[0].body
